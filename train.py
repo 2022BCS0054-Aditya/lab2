1558fda9e314539d40b8c232f0e5257a6084bc78
2022BCS0054-Aditya <aditya22bcs54@iiitkottayam.ac.in>
2026-01-09 12:39:50 +0530

    ride, alpha=10, feature selection by correlation, standard scalerization, train/test split 0.7/0.3

--- train.py
+++ train.py
@@ -29,9 +29,9 @@ scaler = StandardScaler()
 X_processed = pd.DataFrame(scaler.fit_transform(X), columns=X.columns)
 
 # 3. Train the model
-X_train, X_test, y_train, y_test = train_test_split(X_processed, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X_processed, y, test_size=0.3, random_state=42)
 
-model = Ridge(alpha=1.0)
+model = Ridge(alpha=10)
 # model = RandomForestRegressor(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
