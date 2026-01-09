71bef9324ba81b3ee16f589e9e24454eaf8e7af7
2022BCS0054-Aditya <aditya22bcs54@iiitkottayam.ac.in>
2026-01-09 12:42:44 +0530

    random forest, trees=100, depth=5, all features, train/test split 0.7/0.3

--- train.py
+++ train.py
@@ -32,7 +32,7 @@ correlations = df.corr()['quality'].drop('quality')
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # model = Ridge(alpha=10)
-model = RandomForestRegressor(n_estimators=100, random_state=42)
+model = RandomForestRegressor(n_estimators=100, max_depth=5, random_state=42)
 model.fit(X_train, y_train)
 
 # 4. Evaluate the model
